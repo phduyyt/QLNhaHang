e7b0987b0057472a12c2e0044cf0950767201763
ThuTrang21 <thut15794@gmail.com>
2024-08-15 16:49:40 +0700

    update

--- app/src/main/java/com/example/qlnhahang/Tables/EditTable.java
+++ app/src/main/java/com/example/qlnhahang/Tables/EditTable.java
@@ -162,6 +162,7 @@ public class EditTable extends AppCompatActivity {
 
             // Quay lại danh sách bàn ăn
             Intent intent = new Intent(EditTable.this, QLTable.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish(); // Kết thúc Activity hiện tại
 
